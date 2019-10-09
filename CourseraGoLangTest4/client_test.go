bccb17391a3512f8fd4915f8e5fdfead0f5d3dfb
DudinDmitriy <dood_pvs@mail.ru>
2019-10-09 19:42:34 +0300

    Dev 2019_10_08-1

--- CourseraGoLangTest4/client_test.go
+++ CourseraGoLangTest4/client_test.go
@@ -1,10 +1,63 @@
 package main
 
-// код писать тут
-func SearchServer(){
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"sort"
+	"testing"
+)
+
+//Функция заполняет данными массив в зависимости от параметров
+//В функцию передают пустой массив, данные дополнятся в массив
+func GetData(query string, orderFiled string, orderBy int, result []User) {
+
+	f, err := os.Open("./dataset.xml")
+	if err != nil {
+		fmt.Errorf("can`t open file: %s", err)
+	}
+
+	users := []User{}
+	fileContext, err := ioutil.ReadAll(f)
+
+	err = xml.Unmarshal(fileContext, users)
+	if query != "" {
+		sort.Slice(users, func(i, j int) bool { return (users[i].Name + users[i].About) < (users[j].Name + users[j].About) })
+		in := sort.Search(len(users), func(i int) bool { return (users[i].Name + users[i].About) >= query })
+	}
 
 }
 
-func Test{
-	
+// код писать тут
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+
+	//Получаем параметры
+	query := r.FormValue("query")
+	order_filed := r.FormValue("order_field")
+	order_by := r.FormValue("order_by")
+
+	//Получим данные
+	f, err := os.Open("./dataset.xml")
+	if err != nil {
+		fmt.Errorf("can`t open file: %s", err)
+	}
+
+	users := []User{}
+	fileContext, err := ioutil.ReadAll(f)
+	err = xml.Unmarshal(fileContext, users)
+
+	//Поиск данных
+
+}
+
+func Testreqwest1(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	sc := SearchClient{
+		AccessToken: "valodtoken",
+		URL:         ts.URL}
+
 }
