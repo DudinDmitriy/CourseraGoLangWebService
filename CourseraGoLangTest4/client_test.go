246d948c170098802a5251f5a6ac6cc020d94ce2
DudinDmitriy <dood_pvs@mail.ru>
2019-10-11 18:28:13 +0300

    Dev 2019_10_11_1

--- CourseraGoLangTest4/client_test.go
+++ CourseraGoLangTest4/client_test.go
@@ -1,63 +1,232 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"sort"
+	"strconv"
 	"testing"
 )
 
+const (
+	orderIDAgeName = "Id_Age_Name"
+	nameValidToken = "validtoken"
+)
+
+type TestCase struct {
+	Query      string
+	Orderfield string
+	OrderBy    int
+}
+
 //Функция заполняет данными массив в зависимости от параметров
 //В функцию передают пустой массив, данные дополнятся в массив
-func GetData(query string, orderFiled string, orderBy int, result []User) {
+func GetData(query string, orderField string, orderBy int, limit int, offset int, result []User) (err error) {
+
+	if orderField != "" && orderField != orderIDAgeName {
+		err = fmt.Errorf("ErrorBadOrderField")
+		return
+	}
+
+	if orderBy != orderDesc && orderBy != OrderByAsIs && orderBy != OrderByAsc {
+		err = fmt.Errorf("ErrorBadOrderBy")
+		return
+	}
 
 	f, err := os.Open("./dataset.xml")
 	if err != nil {
-		fmt.Errorf("can`t open file: %s", err)
+		err = fmt.Errorf("can`t open file: %s", err)
+		return
 	}
 
-	users := []User{}
-	fileContext, err := ioutil.ReadAll(f)
+	users := make([]User,0)
 
-	err = xml.Unmarshal(fileContext, users)
-	if query != "" {
-		sort.Slice(users, func(i, j int) bool { return (users[i].Name + users[i].About) < (users[j].Name + users[j].About) })
-		in := sort.Search(len(users), func(i int) bool { return (users[i].Name + users[i].About) >= query })
+	fileCon, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
 	}
 
+	type DataRow struct {
+		Id        int    `xml:"id"`
+		FirstName string `xml:"first_name"`
+		LastName  string `xml:"last_name"`
+		Age       int    `xml:"age"`
+		About     string `xml:"about"`
+		Gender    string `xml:"gender"`
+	}
+	type Structdataset struct {
+		Rows []DataRow `xml:"row"`
+	}
+
+	dataset := &Structdataset{}
+
+	xml.Unmarshal(fileCon, dataset)
+
+	for i := 0; i < len(dataset.Rows); i++ {
+		d := dataset.Rows[i]
+		if query == "" || query == (d.FirstName+d.LastName+d.About) {
+
+			usr := User{}
+			usr.Name = d.FirstName + d.LastName
+			usr.Id = d.Id
+			usr.Age = d.Age
+			usr.About = d.About
+			usr.Gender = d.Gender
+
+			users = append(users, usr)
+		}
+	}
+
+	if orderBy == OrderByAsc || orderBy == OrderByDesc {
+
+		type sortfunc func(i, j int) bool
+		var sf sortfunc
+
+		sf = func(i, j int) bool {
+			var k, l int
+
+			if orderBy == OrderByAsc {
+				k = i
+				l = j
+			} else {
+				k = j
+				l = l
+			}
+
+			if users[k].Id < users[l].Id {
+				return true
+			}
+
+			if users[k].Id == users[l].Id && users[k].Age < users[l].Age {
+				return true
+			}
+
+			if users[k].Id == users[l].Id && users[k].Age == users[l].Age && users[k].Name < users[l].Name {
+				return true
+			}
+			return false
+		}
+		sort.Slice(users, sf)
+	}
+
+	for i := limit * (offset - 1); i >= 0 && i < limit*offset && i < len(users); i++ {
+		result = append(result, users[i])
+	}
+	return nil
+}
+
+func GetDataSearchRequest(sr SearchRequest, result []User) (err error) {
+	return GetData(sr.Query, sr.OrderField, sr.OrderBy, sr.Limit, sr.Offset, result)
 }
 
 // код писать тут
 func SearchServer(w http.ResponseWriter, r *http.Request) {
 
-	//Получаем параметры
-	query := r.FormValue("query")
-	order_filed := r.FormValue("order_field")
-	order_by := r.FormValue("order_by")
-
-	//Получим данные
-	f, err := os.Open("./dataset.xml")
-	if err != nil {
-		fmt.Errorf("can`t open file: %s", err)
+	token := r.Header.Get("AccessToken")
+	if token != nameValidToken {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 
-	users := []User{}
-	fileContext, err := ioutil.ReadAll(f)
-	err = xml.Unmarshal(fileContext, users)
+	//Получаем параметры
+	query := r.FormValue("query")
+	orderFiled := r.FormValue("order_field")
+	orderBy, _ := strconv.Atoi(r.FormValue("order_by"))
+	limit, _ := strconv.Atoi(r.FormValue("limit"))
+	offset, _ := strconv.Atoi(r.FormValue("offset"))
 
-	//Поиск данных
+	//Получим данные
+	users := make([]User, 0)
+	err := GetData(query, orderFiled, orderBy, limit, offset, users)
+	if err != nil {
+		if err.Error() == "ErrorBadOrderField" || err.Error() == "ErrorBadOrderBy" {
+			w.WriteHeader(http.StatusBadRequest)
+			errRes := SearchErrorResponse{Error: err.Error()}
+			bytejson, _ := json.Marshal(errRes)
+			io.WriteString(w, string(bytejson))
+			return
+		}
+	}
+
+	bytejson, _ := json.Marshal(users)
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(bytejson))
 
 }
 
-func Testreqwest1(t *testing.T) {
+func TestSearchServer(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
 	sc := SearchClient{
-		AccessToken: "valodtoken",
-		URL:         ts.URL}
+		AccessToken: nameValidToken,
+		URL:         ts.URL,
+	}
 
+	TestCases := []SearchRequest{
+		SearchRequest{
+			Limit:      5,
+			Offset:     3,
+			Query:      "",
+			OrderField: orderIDAgeName,
+			OrderBy:    OrderByDesc,
+		},
+		SearchRequest{
+			Limit:      25,
+			Offset:     1,
+			Query:      "",
+			OrderField: orderIDAgeName,
+			OrderBy:    orderAsc,
+		},
+	}
+	
+	for _, tt := range TestCases {
+		
+		res, err := sc.FindUsers(tt)
+		userseq := make([]User, 0)
+		
+		if err != nil {
+			t.Errorf("Ошибка: %s", err.Error())
+			continue
+		}
+		
+		GetDataSearchRequest(tt, userseq)
+		if !reflect.DeepEqual(res.Users, userseq) {
+			t.Errorf("Для структуры запросы %+v данные различаются", tt)
+		}
+	}
+}
+
+func TestSearchServerToken(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	sc := SearchClient{
+		AccessToken: nameValidToken,
+		URL:         ts.URL,
+	}
+
+	TestCases := []SearchRequest{
+		SearchRequest{
+			Limit:      1,
+			Offset:     1,
+			Query:      "",
+			OrderField: "NotLegal",
+			OrderBy:    orderAsc,
+		},
+	}
+	
+	for _, tt := range TestCases {
+		
+		res, err := sc.FindUsers(tt)
+		
+		if res != nil || err.Error()!= "OrderFeld NotLegal invalid" {
+			t.Errorf("Ошибка: неккорктно отрабатывает ошибку заполнения поля  <OrderField>")
+		}
+	}
 }
